c3a8a100a8ff0f05af136f2e042ecb1d86917fd6
Chrisco <chrisco@theintaweb.net>
2018-09-05 12:53:06 +0800

    final again

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'B3atm3up!!69',
+        'PASSWORD': '',
         'HOST' : 'localhost',
         'PORT' : '5432',
 
